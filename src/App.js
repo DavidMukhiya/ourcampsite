bad570a080b454ddabb701502febbc5bf4b0ec21
DavidMukhiya <sherlock.41v94@gmail.com>
2022-05-16 13:59:56 -0700

    Organize the file and added Header component by adding Navbar code from app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
-//import CampsiteCard from './features/counter/campsites/CampsiteCard';
+import Header from './components/Header';
 import CampsitesList from './features/campsites/CampsitesList';
-//import { CAMPSITES } from './app/shared/CAMPSITES';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
+      <Header/>
       <CampsitesList />
     </div>
   );
